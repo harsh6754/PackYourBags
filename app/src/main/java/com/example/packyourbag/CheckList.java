35af4119518b2a6b753a6b2b6dba1ef29d8ea4fe
harsh6754 <210306105012@paruluniversity.ac.in>
2023-12-14 15:29:51 +0530

    PackYourBags

--- app/src/main/java/com/example/packyourbag/CheckList.java
+++ app/src/main/java/com/example/packyourbag/CheckList.java
@@ -167,7 +167,7 @@ public class CheckList extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 101){
+        if (requestCode == 101) {
             itemsList = database.mainDao().getAll(header);
             updateRecycler(itemsList);
         }
