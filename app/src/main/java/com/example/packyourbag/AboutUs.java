e7f0cf95ed5028e4511c1693b5e57c6bb71b3918
harsh6754 <210306105012@paruluniversity.ac.in>
2023-12-15 10:24:26 +0530

    PackYourBags

--- app/src/main/java/com/example/packyourbag/AboutUs.java
+++ app/src/main/java/com/example/packyourbag/AboutUs.java
@@ -3,12 +3,27 @@ package com.example.packyourbag;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class AboutUs extends AppCompatActivity {
+    ImageView imgYoutube,imgInstagram,imgTwitter;
+    TextView txtEmail,txtWebsiteUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("About Us");
+
+
+        imgYoutube = findViewById(R.id.imgYoutube);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtWebsiteUrl = findViewById(R.id.txtWebsiteUrl);
+        imgInstagram = findViewById(R.id.imgInstagram);
+        imgTwitter = findViewById(R.id.imgtwitter);
+
     }
 }
