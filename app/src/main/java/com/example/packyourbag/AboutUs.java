c06d51a65a6ec3fd3b7b4a103a865065b0541895
harsh6754 <210306105012@paruluniversity.ac.in>
2023-12-16 15:47:39 +0530

    PackYourBags

--- app/src/main/java/com/example/packyourbag/AboutUs.java
+++ app/src/main/java/com/example/packyourbag/AboutUs.java
@@ -2,13 +2,17 @@ package com.example.packyourbag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class AboutUs extends AppCompatActivity {
-    ImageView imgYoutube,imgInstagram,imgTwitter;
-    TextView txtEmail,txtWebsiteUrl;
+    ImageView imgYoutube, imgInstagram, imgTwitter;
+    TextView txtEmail, txtWebsiteUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,71 @@ public class AboutUs extends AppCompatActivity {
         imgInstagram = findViewById(R.id.imgInstagram);
         imgTwitter = findViewById(R.id.imgtwitter);
 
+        imgYoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.youtube.com/channel/UCyOi2-izi7RtEWVmkqhavWg"));
+                startActivity(intent);
+            }
+        });
+
+        imgInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.instagram.com/unique_coder6754/"));
+                startActivity(intent);
+            }
+        });
+
+        imgTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://twitter.com/HarshAg62381240"));
+                startActivity(intent);
+            }
+        });
+
+        txtEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:" + "uniquecoder6754@gmail.com"));
+                    intent.putExtra(Intent.EXTRA_SUBJECT, "From Pack Your Bags");
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    System.out.println(e);
+                }
+            }
+        });
+
+
+        txtWebsiteUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://agrawalharsh.netlify.app/"));
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    System.out.println(e);
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
